a6e784f0895d932d1a1cace17ef346073f6f16d6
Yogesh Sutar <yogeshsutar2003@gmail.com>
2024-11-07 00:05:15 +0530

    Modified Member's OnBoarding Page

--- client/src/pages/OnboardingPage.jsx
+++ client/src/pages/OnboardingPage.jsx
@@ -55,105 +55,141 @@ const OnboardingPage = () => {
                 </h2>
                 <p className="text-center text-gray-500 mb-6">Please provide your basic details to continue.</p>
                 <form onSubmit={handleSubmit} className="space-y-4">
-                    <input
-                        type="text"
-                        placeholder="First Name"
-                        name="firstName"
-                        value={formData.firstName}
-                        onChange={handleChange}
-                        className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-blue-500"
-                        required
-                    />
-                    <input
-                        type="text"
-                        placeholder="Last Name"
-                        name="lastName"
-                        value={formData.lastName}
-                        onChange={handleChange}
-                        className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-blue-500"
-                        required
-                    />
-                    <input
-                        type="date"
-                        name="dob"
-                        value={formData.dob}
-                        onChange={handleChange}
-                        className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-blue-500"
-                        required
-                    />
-                    <div className="flex gap-4">
-                        <label className="flex items-center">
-                            <input
-                                type="radio"
-                                name="gender"
-                                value="male"
-                                checked={formData.gender === 'male'}
-                                onChange={handleChange}
-                                className="mr-2  transform scale-125" 
-                            />
-                            Male
-                        </label>
-                        <label className="flex items-center">
-                            <input
-                                type="radio"
-                                name="gender"
-                                value="female"
-                                checked={formData.gender === 'female'}
-                                onChange={handleChange}
-                                className="mr-2  transform scale-125" 
-                            />
-                            Female
-                        </label>
-                        <label className="flex items-center">
-                            <input
-                                type="radio"
-                                name="gender"
-                                value="other"
-                                checked={formData.gender === 'other'}
-                                onChange={handleChange}
-                                className="mr-2  transform scale-125" 
+                    <div className="flex flex-col gap-1">
+                        <label className="text-sm font-medium text-gray-400">Name</label>
+                        <div className="flex flex-row gap-4">
+                            <div className="flex-1">
+                                <input
+                                    type="text"
+                                    placeholder="First Name"
+                                    name="firstName"
+                                    value={formData.firstName}
+                                    onChange={handleChange}
+                                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-blue-500"
+                                    required
                                 />
-                            Other
-                        </label>
+                            </div>
+                            <div className="flex-1">
+                                <input
+                                    type="text"
+                                    placeholder="Last Name"
+                                    name="lastName"
+                                    value={formData.lastName}
+                                    onChange={handleChange}
+                                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-blue-500"
+                                    required
+                                />
+                            </div>
+                        </div>
                     </div>
-                    <input
-                        type="text"
-                        placeholder="Location"
-                        name="location"
-                        value={formData.location}
-                        onChange={handleChange}
-                        className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-blue-500"
-                        required
-                    />
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        name="password"
-                        value={formData.password}
-                        onChange={handleChange}
-                        className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-blue-500"
-                        required
-                    />
-                    <input
-                        type="password"
-                        placeholder="Confirm Password"
-                        name="confirmPassword"
-                        value={formData.confirmPassword}
-                        onChange={handleChange}
-                        className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-blue-500"
-                        required
-                    />
-                    <input
-                        type="text"
-                        placeholder="Mobile No."
-                        name="mobile"
-                        value={formData.mobile}
-                        onChange={handleMobileChange}
-                        className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-blue-500"
-                        maxLength="10"
-                        title="Please enter a valid 10-digit phone number"
-                        required
-                    />
+
+                    <div className='relative'>
+                        <label htmlFor="dob" className="flex justify-start text-sm font-medium text-gray-400">
+                            Date of Birth
+                        </label>
+                        <div>
+                            <input
+                                type="date"
+                                id="dob"
+                                name="dob"
+                                value={formData.dob}
+                                onChange={handleChange}
+                                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-blue-500"
+                                required
+                            />
+                        </div>
+                    </div>
+                    <div className="flex flex-col items-center gap-2">
+                        <label className="text-sm font-medium text-gray-400">Gender</label>
+                        <div className="flex gap-4">
+                            <label className="flex items-center">
+                                <input
+                                    type="radio"
+                                    name="gender"
+                                    value="male"
+                                    checked={formData.gender === 'male'}
+                                    onChange={handleChange}
+                                    className="mr-2 transform scale-125"
+                                />
+                                Male
+                            </label>
+                            <label className="flex items-center">
+                                <input
+                                    type="radio"
+                                    name="gender"
+                                    value="female"
+                                    checked={formData.gender === 'female'}
+                                    onChange={handleChange}
+                                    className="mr-2 transform scale-125"
+                                />
+                                Female
+                            </label>
+                            <label className="flex items-center">
+                                <input
+                                    type="radio"
+                                    name="gender"
+                                    value="other"
+                                    checked={formData.gender === 'other'}
+                                    onChange={handleChange}
+                                    className="mr-2 transform scale-125"
+                                />
+                                Other
+                            </label>
+                        </div>
+                    </div>
+
+                    <div>
+                        <label className="flex justify-start text-sm font-medium text-gray-400">Location</label>
+                        <input
+                            type="text"
+                            placeholder="Location"
+                            name="location"
+                            value={formData.location}
+                            onChange={handleChange}
+                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-blue-500"
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label className="flex justify-start text-sm font-medium text-gray-400">Mobile No.</label>
+                        <input
+                            type="text"
+                            placeholder="Mobile No."
+                            name="mobile"
+                            value={formData.mobile}
+                            onChange={handleMobileChange}
+                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-blue-500"
+                            maxLength="10"
+                            title="Please enter a valid 10-digit phone number"
+                            required
+                        />
+                    </div>
+                    <div className="flex flex-row gap-4">
+                        <div className="flex-1">
+                            <label className="flex justify-start text-sm font-medium text-gray-400">Password</label>
+                            <input
+                                type="password"
+                                placeholder="Password"
+                                name="password"
+                                value={formData.password}
+                                onChange={handleChange}
+                                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-blue-500"
+                                required
+                            />
+                        </div>
+                        <div className="flex-1">
+                            <label className="flex justify-start text-sm font-medium text-gray-400">Confirm Password</label>
+                            <input
+                                type="password"
+                                placeholder="Confirm Password"
+                                name="confirmPassword"
+                                value={formData.confirmPassword}
+                                onChange={handleChange}
+                                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-blue-500"
+                                required
+                            />
+                        </div>
+                    </div>
                     {error && <p className="text-red-500 text-sm">{error}</p>}
                     <button
                         type="submit"
